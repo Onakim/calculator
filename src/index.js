aa35ab8756bac31910685dacdae1ab20873a9e4f
Lyubov Myetolkina <luba150278@gmail.com>
2022-11-11 18:38:48 +0200

    redux

--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
+const actions = [
+  {
+    type: "CANCEL",
+  },
+  {
+    type: "RESULT",
+  },
+  {
+    type: "FUNC",
+  },
+  {
+    type: "NUMB",
+  },
+];
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>

082032a91fd0bf090f3f5436d32861a87a5a3281
Andrey Mekeda <andreymekeda@gmail.com>
2022-11-30 12:56:20 +0200

    public

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
 function App() {
+  const dispatch = useDispatch();
+  const value = useSelector(state=>state.value);
   return (
     <div className="app">
       <div className="container">
         <h1>Calculator</h1>
         <div className="app__inner">
-          <p className="result">0</p>
+          <p className="result">{value}</p>
           <div className="buttons">
-            <div className="item">+</div>
-            <div className="item">-</div>
-            <div className="item">/</div>
-            <div className="item">*</div>
-            <div className="item">C</div>
-            <div className="item item-result">=</div>
+            <div className="item" onClick={()=>dispatch({type: "FUNC", payload: "PLUS"})}>+</div>
+            <div className="item" onClick={()=>dispatch({type: "FUNC", payload: "MINUS"})}>-</div>
+            <div className="item" onClick={()=>dispatch({type: "FUNC", payload: "DEVIDE"})}>/</div>
+            <div className="item" onClick={()=>dispatch({type: "FUNC", payload: "MULTY"})}>*</div>
+            <div className="item" onClick={()=>dispatch({type: "FUNC", payload: "PERCENT"})}>%</div>
+            <div className="item" onClick={()=>dispatch({type: "FUNC", payload: "ROOT"})}>&#8730;</div>
+            <div className="item" onClick={()=>dispatch({type: "CANCEL"})}>C</div>
+            <div className="item" onClick={()=>dispatch({type: "BACK"})}>back</div>
+            <div className="item item-result" onClick={()=>dispatch({type: "RESULT"})}>=</div>
           </div>
           <div className="numbers">
             {numbers.map((item) => (
-              <div key={`item-${item + 1}`} className="item">
+              <div
+                key={`item-${item + 1}`}
+                className="item"
+                onClick={() => dispatch({ type: "NUMB", payload: item })}
+              >
                 {item}
               </div>
             ))}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,85 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import "./index.css";
 import App from "./App";
 
+const defaultState = {
+  value: "0",
+  firstValue: "0",
+  func: "",
+};
+const actions = [
+  {
+    type: "CANCEL",
+  },
+  {
+    type: "RESULT",
+  },
+  {
+    type: "FUNC",
+  },
+  {
+    type: "NUMB",
+  },
+  {
+    type: "BACK",
+  },
+];
+const getResult = (first, second, func) => {
+  switch (func) {
+    case "PLUS":
+      return (Number(first) + Number(second)).toString();
+    case "MINUS":
+      return (Number(first) - Number(second)).toString();
+    case "MULTY":
+      return (Number(first) * Number(second)).toString();
+    case "DEVIDE":
+      return (Number(first) / Number(second)).toString();
+    case "PERCENT":
+      return (Number(first) / 100 * Number(second)).toString();
+    case "ROOT":
+      return (Math.sqrt(Number(first))).toString();
+    default:
+      return "";
+  }
+};
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case "NUMB":
+      const data =
+        state.value === "0"
+          ? action.payload
+          : `${state.value}${action.payload}`;
+      return { ...state, value: data };
+    case "CANCEL":
+      return { ...state, value: "0" };
+    case "FUNC":
+      if (state.value === "0" && action.payload === "MINUS") {
+        return { ...state, value: "-" };
+      }
+      return {
+        ...state,
+        firstValue: state.value,
+        func: action.payload,
+        value: "0",
+      };
+    case "RESULT":
+      const res = getResult(state.firstValue, state.value, state.func);
+      return { ...state, value: res, firstValue: "0", func: "" };
+    case "BACK":
+      const backData = state.value.length === 1 ? "0" : state.value.slice(0, state.value.length - 1);
+      return { ...state, value: backData };
+    default:
+      return state;
+  }
+};
+export const store = createStore(reducer, composeWithDevTools());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
